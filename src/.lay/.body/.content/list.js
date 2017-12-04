fa45e930754c7c5869cfa21c8392b69eabfc1a5f
Alexandru Thirtheu <alex_giusi_tiri2@yahoo.com>
2017-12-03 22:25:35 -0500

    Second, workable, version

--- src/.lay/.body/.content/list.js
+++ src/.lay/.body/.content/list.js
@@ -1,6 +1,8 @@
 //$.get ('srv/eg/ndx.html').done (function (data, status, jqxhr) { $('#content_list').append (data); });
 var src = get ('src');
 if (src == null)
+	//src = encodeURIComponent ('src/eg/ndx.html');
 	src = 'src/eg/blank.html';
 
+//$.get (decodeURIComponent (src)).done (function (data, status, jqxhr) { $('#content_list').append (data); });
 $.get (src).done (function (data, status, jqxhr) { $('#content_list').append (data); });
